16ccb6d7ed0acd6f7e1d4565c1a0c180f2339740
Mykhail <gorgefraggggg@gmail.com>
2024-12-10 02:08:26 +0300

    asdsaddsaasdsad

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,11 +4,9 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5173, // Указываем порт для dev-сервера
+    port: 5173,
   },
-  build: {
-    rollupOptions: {
-      external: ["react-router-dom"], // Указываем react-router-dom как внешнюю зависимость
-    },
+  optimizeDeps: {
+    include: ["react-router-dom"], // Указываем зависимость для оптимизации
   },
 });
